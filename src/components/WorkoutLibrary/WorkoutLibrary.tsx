d39ef8c1b8df5fd9c7d7a9be83af6967acbbc7a9
Hardik Vala <hardikvala24@gmail.com>
2023-09-07 17:24:37 -0700

    Add workout previews.

--- src/components/WorkoutLibrary/WorkoutLibrary.tsx
+++ src/components/WorkoutLibrary/WorkoutLibrary.tsx
@@ -41,6 +41,9 @@ export function WorkoutLibrary({ onSignOut }: WorkoutLibraryProps) {
               <List.Subheader>Cardio</List.Subheader>
               <List.Item
                 title="Rower"
+                description="Rower, Cool Down"
+                descriptionNumberOfLines={1}
+                descriptionEllipsizeMode="tail"
                 left={() => <List.Icon icon="rowing" />}
                 onPress={() => setSelectedWorkout("rower")}
                 style={workoutLibraryStyles.workoutListItem}
@@ -48,18 +51,27 @@ export function WorkoutLibrary({ onSignOut }: WorkoutLibraryProps) {
               <List.Subheader>Strength</List.Subheader>
               <List.Item
                 title="Push"
+                description="Flat Bench Press, Angled Bench Press"
+                descriptionNumberOfLines={1}
+                descriptionEllipsizeMode="tail"
                 left={() => <List.Icon icon="weight-lifter" />}
                 onPress={() => setSelectedWorkout("push")}
                 style={workoutLibraryStyles.workoutListItem}
               />
               <List.Item
                 title="Pull"
+                description="Lat Pull-ups, Tricep Dips, Bent Over Rows"
+                descriptionNumberOfLines={1}
+                descriptionEllipsizeMode="tail"
                 left={() => <List.Icon icon="weight-lifter" />}
                 onPress={() => setSelectedWorkout("pull")}
                 style={workoutLibraryStyles.workoutListItem}
               />
               <List.Item
                 title="Legs"
+                description="Wide-stance Squats, Hip Thrusts, Bulgarian Split Squats"
+                descriptionNumberOfLines={1}
+                descriptionEllipsizeMode="tail"
                 left={() => <List.Icon icon="weight-lifter" />}
                 onPress={() => setSelectedWorkout("legs")}
                 style={workoutLibraryStyles.workoutListItem}
@@ -67,12 +79,18 @@ export function WorkoutLibrary({ onSignOut }: WorkoutLibraryProps) {
               <List.Subheader>PT</List.Subheader>
               <List.Item
                 title="PT routine"
+                description="Chicken Wings, Side-to-sides, Single-leg Bridges"
+                descriptionNumberOfLines={1}
+                descriptionEllipsizeMode="tail"
                 left={() => <List.Icon icon="yoga" />}
                 onPress={() => setSelectedWorkout("ptRoutine")}
                 style={workoutLibraryStyles.workoutListItem}
               />
               <List.Item
                 title="Stretching"
+                description="Standing Calf Stretch, Standing IT Band Stretch"
+                descriptionNumberOfLines={1}
+                descriptionEllipsizeMode="tail"
                 left={() => <List.Icon icon="yoga" />}
                 onPress={() => setSelectedWorkout("stretching")}
                 style={workoutLibraryStyles.workoutListItem}
