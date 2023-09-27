1080814cfbbffd942d68e467a39f04482efaf9e2
Hardik Vala <hardikvala24@gmail.com>
2023-09-26 20:51:47 -0700

    Add GET endpoint for workout history.

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,16 +4,26 @@ const app = express();
 
 app.use(express.json()); 
 
-// POST endpoint
-app.post('/api/post', (req, res) => {
+app.get("/api/v1/workout/history", (req, res) => {
+  const { workoutKey } = req.query;
+
+  if (!workoutKey) {
+    return res.status(400).json({
+      error: "workoutkey is required" 
+    });
+  }
+
+  res.send("GET request received!");
+});
+
+app.post("/api/v1/workout/save", (req, res) => {
   const body = req.body;
   
   // Do something with body...
   
-  res.send('POST request received!');
+  res.send("POST request received!");
 });
 
-// Start server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
