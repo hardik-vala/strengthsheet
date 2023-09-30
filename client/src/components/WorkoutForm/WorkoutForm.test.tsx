85c94de41bfd02d7413035f1392e655264170147
Hardik Vala <hardikvala24@gmail.com>
2023-09-29 21:21:28 -0700

    Update tests.

--- client/src/components/WorkoutForm/WorkoutForm.test.tsx
+++ client/src/components/WorkoutForm/WorkoutForm.test.tsx
@@ -1,12 +1,318 @@
-import { act, fireEvent, render, screen } from "@testing-library/react-native";
+import { act, fireEvent, render, screen, waitFor } from "@testing-library/react-native";
+import { parse as parseDate } from "date-fns";
 import React from "react";
 import mockSafeAreaContext from "react-native-safe-area-context/jest/mock";
+import { WORKOUT_TEMPLATE_REGISTRY } from "../../data/registry";
+import { SetType, WorkoutValueKey } from "../../models/Workout/Core";
+import { WorkoutHistory } from "../../models/Workout/WorkoutHistory";
+import { WorkoutTemplate } from "../../models/Workout/WorkoutTemplate";
 
 jest.useFakeTimers().setSystemTime(new Date("2023-08-27"));
 
 jest.mock("react-native-safe-area-context", () => mockSafeAreaContext);
 
-import { WORKOUT_TEMPLATE_REGISTRY } from "../../data/registry";
+const MOCK_WORKOUT_HISTORY: { [k: string]: WorkoutHistory } = {
+  rowing_machine: {
+    workoutTemplate: WORKOUT_TEMPLATE_REGISTRY["rowing_machine"],
+    records: [
+      {
+        startTimestamp: convertToDateObj("08/25/2023 20:04"),
+        exercises: [
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "meters"
+            ),
+            value: "1000",
+          },
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "time"
+            ),
+            value: "00:30:00",
+          },
+        ],
+      },
+      {
+        startTimestamp: convertToDateObj("08/25/2023 20:04"),
+        exercises: [
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "meters"
+            ),
+            value: "1000",
+          },
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "time"
+            ),
+            value: "00:30:00",
+          },
+        ],
+      },
+      {
+        startTimestamp: convertToDateObj("08/29/2023 21:19"),
+        exercises: [
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "meters"
+            ),
+            value: "2000",
+          },
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "time"
+            ),
+            value: "00:31:00",
+          },
+        ],
+      },
+      {
+        startTimestamp: convertToDateObj("08/29/2023 21:20"),
+        exercises: [
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "meters"
+            ),
+            value: "3000",
+          },
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "time"
+            ),
+            value: "00:32:00",
+          },
+        ],
+      },
+      {
+        startTimestamp: convertToDateObj("09/02/2023 21:10"),
+        exercises: [
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "meters"
+            ),
+            value: "4000",
+          },
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "time"
+            ),
+            value: "00:33:00",
+          },
+        ],
+      },
+    ],
+  },
+  legs: {
+    workoutTemplate: WORKOUT_TEMPLATE_REGISTRY["legs"],
+    records: [
+      {
+        startTimestamp: convertToDateObj("09/20/2023 20:00"),
+        exercises: [
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              1,
+              SetType.Warmup,
+              "lb"
+            ),
+            value: "45",
+          },
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              1,
+              SetType.Warmup,
+              "reps"
+            ),
+            value: "15",
+          },
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              2,
+              SetType.Warmup,
+              "lb"
+            ),
+            value: "65",
+          },
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              2,
+              SetType.Warmup,
+              "reps"
+            ),
+            value: "15",
+          },
+          {
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              1,
+              SetType.Working,
+              "lb"
+            ),
+            value: "25",
+          },
+          {
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              1,
+              SetType.Working,
+              "reps"
+            ),
+            value: "20",
+          },
+          {
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              2,
+              SetType.Working,
+              "lb"
+            ),
+            value: "25",
+          },
+          {
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              2,
+              SetType.Working,
+              "reps"
+            ),
+            value: "20",
+          },
+        ],
+      },
+      {
+        startTimestamp: convertToDateObj("09/19/2023 20:00"),
+        exercises: [
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              1,
+              SetType.Warmup,
+              "lb"
+            ),
+            value: "45",
+          },
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              1,
+              SetType.Warmup,
+              "reps"
+            ),
+            value: "15",
+          },
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              2,
+              SetType.Warmup,
+              "lb"
+            ),
+            value: "65",
+          },
+          {
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              2,
+              SetType.Warmup,
+              "reps"
+            ),
+            value: "15",
+          },
+          {
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              1,
+              SetType.Working,
+              "lb"
+            ),
+            value: "20",
+          },
+          {
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              1,
+              SetType.Working,
+              "reps"
+            ),
+            value: "20",
+          },
+          {
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              2,
+              SetType.Working,
+              "lb"
+            ),
+            value: "20",
+          },
+          {
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              2,
+              SetType.Working,
+              "reps"
+            ),
+            value: "20",
+          },
+        ],
+      },
+    ],
+  },
+};
+
+function convertToDateObj(dateStr: string) {
+  return parseDate(dateStr, "MM/dd/yyyy HH:mm", new Date());
+}
+
+import { WORKOUT_HISTORY_PROVIDER } from "../../providers/WorkoutHistoryProvider";
+
+WORKOUT_HISTORY_PROVIDER.getWorkoutHistory = jest.fn().mockImplementation(
+  async (workoutTemplate: WorkoutTemplate): Promise<WorkoutHistory> => {
+    return MOCK_WORKOUT_HISTORY[workoutTemplate.key];
+  }
+);
+
 import { WorkoutForm } from "./WorkoutForm";
 
 describe("WorkoutForm", () => {
@@ -24,6 +330,10 @@ describe("WorkoutForm", () => {
       />
     );
 
+    await waitFor(() => {
+      screen.getByPlaceholderText("4000");
+    });
+
     expect(screen).toMatchSnapshot();
   });
 
@@ -34,6 +344,10 @@ describe("WorkoutForm", () => {
         onBack={onBack}
       />
     );
+
+    await waitFor(() => {
+      screen.getByPlaceholderText("65");
+    });
 
     expect(screen).toMatchSnapshot();
   });
