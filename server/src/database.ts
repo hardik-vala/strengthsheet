895a797329e6c729f77704709149f8d89b5fbe1a
Hardik Vala <hardikvala24@gmail.com>
2023-10-01 20:01:53 -0700

    Format.

--- server/src/database.ts
+++ server/src/database.ts
@@ -15,65 +15,115 @@ export const WORKOUT_HISTORY_TABLE: { [k: string]: WorkoutHistoryTableRow } = {
         startTimestamp: convertToDateObj("08/25/2023 20:04"),
         exercises: [
           {
-            key: WorkoutValueKey.createFromExercise("rowing_machine", 1, SetType.Working, "meters"),
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "meters"
+            ),
             value: "1000",
           },
           {
-            key: WorkoutValueKey.createFromExercise("rowing_machine", 1, SetType.Working, "time"),
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "time"
+            ),
             value: "00:30:00",
-          }
+          },
         ],
       },
       {
         startTimestamp: convertToDateObj("08/25/2023 20:04"),
         exercises: [
           {
-            key: WorkoutValueKey.createFromExercise("rowing_machine", 1, SetType.Working, "meters"),
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "meters"
+            ),
             value: "1000",
           },
           {
-            key: WorkoutValueKey.createFromExercise("rowing_machine", 1, SetType.Working, "time"),
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "time"
+            ),
             value: "00:30:00",
-          }
+          },
         ],
       },
       {
         startTimestamp: convertToDateObj("08/29/2023 21:19"),
         exercises: [
           {
-            key: WorkoutValueKey.createFromExercise("rowing_machine", 1, SetType.Working, "meters"),
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "meters"
+            ),
             value: "2000",
           },
           {
-            key: WorkoutValueKey.createFromExercise("rowing_machine", 1, SetType.Working, "time"),
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "time"
+            ),
             value: "00:31:00",
-          }
+          },
         ],
       },
       {
         startTimestamp: convertToDateObj("08/29/2023 21:20"),
         exercises: [
           {
-            key: WorkoutValueKey.createFromExercise("rowing_machine", 1, SetType.Working, "meters"),
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "meters"
+            ),
             value: "3000",
           },
           {
-            key: WorkoutValueKey.createFromExercise("rowing_machine", 1, SetType.Working, "time"),
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "time"
+            ),
             value: "00:32:00",
-          }
+          },
         ],
       },
       {
         startTimestamp: convertToDateObj("09/02/2023 21:10"),
         exercises: [
           {
-            key: WorkoutValueKey.createFromExercise("rowing_machine", 1, SetType.Working, "meters"),
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "meters"
+            ),
             value: "4000",
           },
           {
-            key: WorkoutValueKey.createFromExercise("rowing_machine", 1, SetType.Working, "time"),
+            key: WorkoutValueKey.createFromExercise(
+              "rowing_machine",
+              1,
+              SetType.Working,
+              "time"
+            ),
             value: "00:33:00",
-          }
+          },
         ],
       },
     ],
@@ -85,35 +135,79 @@ export const WORKOUT_HISTORY_TABLE: { [k: string]: WorkoutHistoryTableRow } = {
         startTimestamp: convertToDateObj("09/20/2023 20:00"),
         exercises: [
           {
-            key: WorkoutValueKey.createFromExercise("squat_wide_stance_barbell", 1, SetType.Warmup, "lb"),
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              1,
+              SetType.Warmup,
+              "lb"
+            ),
             value: "45",
           },
           {
-            key: WorkoutValueKey.createFromExercise("squat_wide_stance_barbell", 1, SetType.Warmup, "reps"),
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              1,
+              SetType.Warmup,
+              "reps"
+            ),
             value: "15",
           },
           {
-            key: WorkoutValueKey.createFromExercise("squat_wide_stance_barbell", 2, SetType.Warmup, "lb"),
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              2,
+              SetType.Warmup,
+              "lb"
+            ),
             value: "65",
           },
           {
-            key: WorkoutValueKey.createFromExercise("squat_wide_stance_barbell", 2, SetType.Warmup, "reps"),
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              2,
+              SetType.Warmup,
+              "reps"
+            ),
             value: "15",
           },
           {
-            key: WorkoutValueKey.createFromCircuit("hip_thrust_circuit", "hip_thrust_single_leg", 1, SetType.Working, "lb"),
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              1,
+              SetType.Working,
+              "lb"
+            ),
             value: "25",
           },
           {
-            key: WorkoutValueKey.createFromCircuit("hip_thrust_circuit", "hip_thrust_single_leg", 1, SetType.Working, "reps"),
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              1,
+              SetType.Working,
+              "reps"
+            ),
             value: "20",
           },
           {
-            key: WorkoutValueKey.createFromCircuit("hip_thrust_circuit", "hip_thrust_single_leg", 2, SetType.Working, "lb"),
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              2,
+              SetType.Working,
+              "lb"
+            ),
             value: "25",
           },
           {
-            key: WorkoutValueKey.createFromCircuit("hip_thrust_circuit", "hip_thrust_single_leg", 2, SetType.Working, "reps"),
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              2,
+              SetType.Working,
+              "reps"
+            ),
             value: "20",
           },
         ],
@@ -122,54 +216,98 @@ export const WORKOUT_HISTORY_TABLE: { [k: string]: WorkoutHistoryTableRow } = {
         startTimestamp: convertToDateObj("09/19/2023 20:00"),
         exercises: [
           {
-            key: WorkoutValueKey.createFromExercise("squat_wide_stance_barbell", 1, SetType.Warmup, "lb"),
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              1,
+              SetType.Warmup,
+              "lb"
+            ),
             value: "45",
           },
           {
-            key: WorkoutValueKey.createFromExercise("squat_wide_stance_barbell", 1, SetType.Warmup, "reps"),
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              1,
+              SetType.Warmup,
+              "reps"
+            ),
             value: "15",
           },
           {
-            key: WorkoutValueKey.createFromExercise("squat_wide_stance_barbell", 2, SetType.Warmup, "lb"),
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              2,
+              SetType.Warmup,
+              "lb"
+            ),
             value: "65",
           },
           {
-            key: WorkoutValueKey.createFromExercise("squat_wide_stance_barbell", 2, SetType.Warmup, "reps"),
+            key: WorkoutValueKey.createFromExercise(
+              "squat_wide_stance_barbell",
+              2,
+              SetType.Warmup,
+              "reps"
+            ),
             value: "15",
           },
           {
-            key: WorkoutValueKey.createFromCircuit("hip_thrust_circuit", "hip_thrust_single_leg", 1, SetType.Working, "lb"),
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              1,
+              SetType.Working,
+              "lb"
+            ),
             value: "20",
           },
           {
-            key: WorkoutValueKey.createFromCircuit("hip_thrust_circuit", "hip_thrust_single_leg", 1, SetType.Working, "reps"),
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              1,
+              SetType.Working,
+              "reps"
+            ),
             value: "20",
           },
           {
-            key: WorkoutValueKey.createFromCircuit("hip_thrust_circuit", "hip_thrust_single_leg", 2, SetType.Working, "lb"),
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              2,
+              SetType.Working,
+              "lb"
+            ),
             value: "20",
           },
           {
-            key: WorkoutValueKey.createFromCircuit("hip_thrust_circuit", "hip_thrust_single_leg", 2, SetType.Working, "reps"),
+            key: WorkoutValueKey.createFromCircuit(
+              "hip_thrust_circuit",
+              "hip_thrust_single_leg",
+              2,
+              SetType.Working,
+              "reps"
+            ),
             value: "20",
           },
         ],
       },
-    ]
+    ],
   },
   pull: {
     workoutKey: "pull",
     records: [],
-  }
-}
+  },
+};
 
 export function serializeWorkoutHistoryTableRow(row: WorkoutHistoryTableRow) {
   return {
     ...row,
-    records: row.records.map(record => ({
+    records: row.records.map((record) => ({
       ...record,
-      startTimestamp: formatDate(record.startTimestamp, "MM/dd/yyyy HH:mm")
-    }))
+      startTimestamp: formatDate(record.startTimestamp, "MM/dd/yyyy HH:mm"),
+    })),
   };
 }
 
