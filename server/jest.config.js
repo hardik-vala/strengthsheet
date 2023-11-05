904bf6234faed4a8e47a4658967efbcd1b6b64ca
Hardik Vala <hardikvala24@gmail.com>
2023-11-04 20:56:53 -0700

    Setup testing.

---
+++ server/jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@models/(.*)$': '<rootDir>/../client/src/models/$1' 
+  }
+};
