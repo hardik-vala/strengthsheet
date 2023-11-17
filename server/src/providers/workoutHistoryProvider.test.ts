c2341f7fe6b5ba8b11dabcc7638ae8b3fabbf3b9
Hardik Vala <hardikvala24@gmail.com>
2023-11-16 21:23:18 -0800

    Update tests.

--- server/src/providers/workoutHistoryProvider.test.ts
+++ server/src/providers/workoutHistoryProvider.test.ts
@@ -1,77 +1,118 @@
 import { SetType, WorkoutValueKey } from "@models/Workout/Core";
 
-const mockAppendRowToGoogleSheet = jest.fn();
+const mockAppendRowsToGoogleSheet = jest.fn();
+const mockCreateSheet = jest.fn();
+const mockSheetExists = jest.fn();
+mockSheetExists.mockResolvedValueOnce(false).mockResolvedValueOnce(true);
 
 jest.mock("./sheetProvider", () => {
   return {
     SHEET_PROVIDER: {
-      appendRowToGoogleSheet: mockAppendRowToGoogleSheet,
+      appendRowsToGoogleSheet: mockAppendRowsToGoogleSheet,
+      createSheet: mockCreateSheet,
+      sheetExists: mockSheetExists,
     },
   };
 });
 
-
 import { WorkoutHistoryRecord } from "@models/Workout/WorkoutHistory";
 import { WORKOUT_HISTORY_PROVIDER } from "./workoutHistoryProvider";
 
+const ACCESS_TOKEN = "foo.accessToken";
+const SPREADSHEET_ID = "1-wL-dRJYZkZ-uVpoBSuGeSFEzg_ZWVKFwLTv8RgbX7o";
+const WORKOUT_KEY = "rowing_machine";
+const SHEET_ID = "Rowing (Machine)";
+const RECORD: WorkoutHistoryRecord = {
+  startTimestamp: new Date("2023-08-28"),
+  elapsedTime: 1800000,
+  exercises: [
+    {
+      key: WorkoutValueKey.createFromExercise(
+        "rowing_machine",
+        1,
+        SetType.Warmup,
+        "meters"
+      ),
+      value: "1000",
+    },
+    {
+      key: WorkoutValueKey.createFromExercise(
+        "rowing_machine",
+        1,
+        SetType.Warmup,
+        "time"
+      ),
+      value: "30:00",
+    },
+    {
+      key: WorkoutValueKey.createFromExercise(
+        "rowing_machine",
+        1,
+        SetType.Working,
+        "meters"
+      ),
+      value: "2000",
+    },
+    {
+      key: WorkoutValueKey.createFromExercise(
+        "rowing_machine",
+        1,
+        SetType.Working,
+        "time"
+      ),
+      value: "60:00",
+    },
+  ],
+};
+
 describe("WORKOUT_HISTORY_PROVIDER", () => {
   describe("appendRecordToWorkoutHistory", () => {
-    it("appends the record to a sheet", () => {
-      const accessToken = "foo.accessToken";
-      const record: WorkoutHistoryRecord = {
-        startTimestamp: new Date("2023-08-28"),
-        elapsedTime: 1800000,
-        exercises: [
-          {
-            key: WorkoutValueKey.createFromExercise(
-              "rowing_machine",
-              1,
-              SetType.Warmup,
-              "meters"
-            ),
-            value: "1000",
-          },
-          {
-            key: WorkoutValueKey.createFromExercise(
-              "rowing_machine",
-              1,
-              SetType.Warmup,
-              "time"
-            ),
-            value: "30:00",
-          },
-          {
-            key: WorkoutValueKey.createFromExercise(
-              "rowing_machine",
-              1,
-              SetType.Working,
-              "meters"
-            ),
-            value: "2000",
-          },
-          {
-            key: WorkoutValueKey.createFromExercise(
-              "rowing_machine",
-              1,
-              SetType.Working,
-              "time"
-            ),
-            value: "60:00",
-          },
-        ],
-      };
-
-      WORKOUT_HISTORY_PROVIDER.appendRecordToWorkoutHistory(
-        accessToken,
-        "rowing_machine",
-        record
+    it("appends the record to a new sheet if the sheet doesn't already exist", async () => {
+      await WORKOUT_HISTORY_PROVIDER.appendRecordToWorkoutHistory(
+        ACCESS_TOKEN,
+        WORKOUT_KEY,
+        RECORD
       );
 
-      expect(mockAppendRowToGoogleSheet).toHaveBeenCalledWith(
-        accessToken,
-        "1-wL-dRJYZkZ-uVpoBSuGeSFEzg_ZWVKFwLTv8RgbX7o",
-        "Rowing (Machine)",
-        ["08/27/2023", "17:00", "30:00", "1000", "30:00", "2000", "60:00"]
+      expect(mockSheetExists).toHaveBeenCalled();
+      expect(mockCreateSheet).toHaveBeenCalledWith(
+        ACCESS_TOKEN,
+        SPREADSHEET_ID,
+        SHEET_ID
+      );
+      expect(mockAppendRowsToGoogleSheet).toHaveBeenCalledWith(
+        ACCESS_TOKEN,
+        SPREADSHEET_ID,
+        SHEET_ID,
+        [
+          [
+            "Date",
+            "Start Time",
+            "Elapsed Time",
+            "Rowing (Machine) : Set 1 (Warmup) : meters",
+            "Rowing (Machine) : Set 1 (Warmup) : time",
+            "Rowing (Machine) : Set 1 (Working) : meters",
+            "Rowing (Machine) : Set 1 (Working) : time",
+          ],
+          ["08/27/2023", "17:00", "30:00", "1000", "30:00", "2000", "60:00"],
+        ]
+      );
+    });
+
+    it("appends the record to an existing sheet", async () => {
+      await WORKOUT_HISTORY_PROVIDER.appendRecordToWorkoutHistory(
+        ACCESS_TOKEN,
+        WORKOUT_KEY,
+        RECORD
+      );
+
+      expect(mockSheetExists).toHaveBeenCalled();
+      expect(mockCreateSheet).toHaveBeenCalledTimes(1);
+      expect(mockAppendRowsToGoogleSheet).toHaveBeenCalledWith(
+        ACCESS_TOKEN,
+        SPREADSHEET_ID,
+        SHEET_ID,
+        [["08/27/2023", "17:00", "30:00", "1000", "30:00", "2000", "60:00"]]
       );
     });
   });
